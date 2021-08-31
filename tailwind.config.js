9990276519526e546d20c71370aebb3ac548489e
Mateus Cristian <mateus_cristian@outlook.com>
2021-08-30 22:21:49 -0300

    [OK]Configurações do extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,36 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18d760',
+        'purple-main': '#2d46b9',
+        'green-main': '1ed760'
+      },
+      backgroundImage: theme => ({
+        'spotify-theme': "url('/img/bursts.svg')",
+        'spotify-theme-mobile': "url('/img/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
